3bb82c7bd69092c5673abb52150df1eac2a05fdc
Dave Brosius <dbrosius@mebigfatguy.com>
2011-11-10 00:11:32 -0500

    when a new column family is added, refetch the KsDef as well

--- src/com/mebigfatguy/clytemnestra/controllers/ColumnFamiliesController.java
+++ src/com/mebigfatguy/clytemnestra/controllers/ColumnFamiliesController.java
@@ -52,6 +52,7 @@ public class ColumnFamiliesController implements Controller, ListSelectionListen
 	@Override
 	public void refresh(Cassandra.Client client) {
         try {
+        	keySpace = client.describe_keyspace(keySpace.getName());
             final List<CfDef> columnFamilies = (client == null) ? new ArrayList<CfDef>() : keySpace.getCf_defs();
             SwingUtilities.invokeLater(new Runnable() {
                 @Override
