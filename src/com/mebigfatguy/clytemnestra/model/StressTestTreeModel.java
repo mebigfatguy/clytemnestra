012e47ba43d8fa70216f0c54c75ae0c8b29b94a3
Dave Brosius <dbrosius@mebigfatguy.com>
2012-01-29 01:00:08 -0500

    stub in stress data

---
+++ src/com/mebigfatguy/clytemnestra/model/StressTestTreeModel.java
@@ -0,0 +1,80 @@
+/*
+ * clytemnastra - a simple gui explorer and stresstool for cassandra
+ * Copyright 2011-2012 MeBigFatGuy.com
+ * Copyright 2011-2012 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.clytemnestra.model;
+
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+import com.mebigfatguy.clytemnestra.StressTestData;
+
+public class StressTestTreeModel implements TreeModel {
+
+	private static final Object ROOT = new Object();
+	private StressTestData stressData;
+	
+	public StressTestTreeModel() {
+		stressData = new StressTestData();
+	}
+	
+	public StressTestTreeModel(URL u) {
+	}
+	
+	public void toStream(OutputStream os) {
+	}
+	
+	@Override
+	public Object getRoot() {
+		return ROOT;
+	}
+
+	@Override
+	public Object getChild(Object parent, int index) {
+		return null;
+	}
+
+	@Override
+	public int getChildCount(Object parent) {
+		return 0;
+	}
+
+	@Override
+	public boolean isLeaf(Object node) {
+		return false;
+	}
+
+	@Override
+	public void valueForPathChanged(TreePath path, Object newValue) {
+	}
+
+	@Override
+	public int getIndexOfChild(Object parent, Object child) {
+		return 0;
+	}
+
+	@Override
+	public void addTreeModelListener(TreeModelListener l) {
+	}
+
+	@Override
+	public void removeTreeModelListener(TreeModelListener l) {
+	}
+}
